f119a5d6b2fabacaeb9df611c82846184ced8b1f
Sampson Obenembot <sampson.obenembot@hellogroup.co.za>
2025-01-14 23:21:53 +0200

    init

---
+++ src/main/java/za/co/medici/MedicibeckendApplication.java
@@ -0,0 +1,13 @@
+package za.co.medici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicibeckendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicibeckendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/medici/MedicibeckendApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.medici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicibeckendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
